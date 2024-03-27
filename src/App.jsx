92301ceaf1ba0f26577e25d7eac732d78005b0c2
Aakarshit07 <aakarshit070@gmail.com>
2024-03-27 17:56:02 +0530

    Initial project Solution

---
+++ src/App.jsx
@@ -0,0 +1,61 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [firstname, setFirstname] = useState("");
+  const [lastname, setLastname] = useState("");
+  const [fullname, setFullname] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if(firstname && lastname) {
+      setFullname(`${firstname} ${lastname}`);
+      setFirstname("");
+      setLastname("");
+    //   console.log("firstname",firstname);
+    //   console.log("lastname", lastname);
+    //   console.log("Previous fullname State: ", fullname);
+    } 
+  }
+
+  return (
+    <>
+      <div className='app'>
+        <h1>Full Name Display</h1>
+        <form action="" className='userform'>
+          <label htmlFor="firstname">First Name:</label>
+          <input 
+            type="text" 
+            id="firstname"
+            name='firstname'
+            required
+            value={firstname}
+            onChange={(e)=> {
+              setFirstname(e.target.value);
+            }}  
+          />
+
+          <label htmlFor="lastname">Last Name:</label>
+          <input 
+            type="text"
+            name='lastname' 
+            id="lastname"
+            required
+            value={lastname}
+            onChange={(e) => {
+              setLastname(e.target.value);
+            }}
+          />
+          <button 
+            type='button'
+            onClick={handleSubmit}
+            >
+            Submit
+          </button>
+        </form>
+        <p>Full Name: {fullname}</p>
+      </div>
+    </>
+  )
+}
+
+export default App;
